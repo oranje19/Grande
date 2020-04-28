003b8a5782a0a1ca8495e3aaf5857601e21c16a1
oranje19 <jasonwintery@gmail.com>
2020-04-28 11:19:30 -0700

    sessionReducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -26,7 +26,6 @@ export const signup = user => dispatch => (
     ))
 );
 
-
 export const login = user => dispatch => (
     APIUtil.login(user).then(user => (
         dispatch(receiveCurrentUser(user))
