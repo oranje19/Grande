a3cc2439b0842c6d4a31cdb0f79571c22c636572
oranje19 <jasonwintery@gmail.com>
2020-04-29 13:42:47 -0700

    Added modal

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import GreetingContainer from './greeting/greeting_container';
-import LoginFormContainer from './session_form/login_form_container'
-import SignupFormContainer from './session_form/signup_form_container'
+import LoginFormContainer from './session/login_form_container'
+import SignupFormContainer from './session/signup_form_container'
 import {
     Route,
     Redirect,
