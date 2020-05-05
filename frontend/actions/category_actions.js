40aa919572afd1eef30badd55a9d8e25888a901f
oranje19 <jasonwintery@gmail.com>
2020-05-04 22:40:27 -0700

    Added category and story actions

---
+++ frontend/actions/category_actions.js
@@ -0,0 +1,26 @@
+import * as CategoriesApiUtil from '../util/category_util';
+
+export const RECEIVE_ALL_CATEGORIES = 'RECEIVE_ALL_CATEGORIES';
+export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY';
+
+const receiveAllCategories = categories => ({
+    type: RECEIVE_ALL_CATEGORIES,
+    categories
+});
+
+const receiveCategory = category => ({
+    type: RECEIVE_CATEGORY,
+    category
+});
+
+export const requestAllCategories = () => dispatch => (
+    CategoriesApiUtil.fetchAllCategories().then((categories) => (
+        dispatch(receiveAllCategories(categories))
+    ))
+);
+
+export const requestCategory = category => dispatch => (
+    CategoriesApiUtil.fetchCategory(category).then((category) => (
+        dispatch(receiveCategory(category))
+    ))
+);
---
+++ frontend/actions/story_actions.js
@@ -0,0 +1,64 @@
+import * as StoryApiUtil from '../util/story_util';
+
+import { receiveErrors } from './error_actions';
+
+export const RECEIVE_STORIES = "RECEIVE_STORIES";
+export const RECEIVE_STORY = "RECEIVE_STORY";
+export const REMOVE_STORY = "REMOVE_STORY";
+
+const receiveStories = stories => ({
+    type: RECEIVE_STORIES,
+    stories
+});
+
+const receiveStory = story => ({
+    type: RECEIVE_STORY,
+    story
+});
+
+const removeStory = storyId => ({
+    type: REMOVE_STORY,
+    storyId
+});
+
+export const requestAllStories = () => dispatch => (
+    StoryApiUtil.fetchAllStories().then((stories) => (
+        dispatch(receiveStories(stories))
+    ))
+);
+
+export const requestCategoryStories = categoryId => dispatch => (
+    StoryApiUtil.fetchCategoryStories(categoryId).then((stories) => (
+        dispatch(receiveStories(stories))
+    ))
+);
+
+export const requestStory = storyId => dispatch => (
+    StoryApiUtil.fetchStory(storyId).then((story) => (
+        dispatch(receiveStory(story))
+    ))
+);
+
+export const createStory = story => dispatch => (
+    StoryApiUtil.createStory(story).then((story) => (
+        dispatch(receiveStory(story))
+    ), (errors) => (
+        dispatch(receiveErrors(errors.responseJSON))
+    ))
+);
+
+export const updateStory = story => dispatch => (
+    StoryApiUtil.updateStory(story).then((story) => (
+        dispatch(receiveStory(story))
+    ), (errors) => (
+        dispatch(receiveErrors(errors.responseJSON))
+    ))
+);
+
+export const deleteStory = storyId => dispatch => (
+    StoryApiUtil.deleteStory(storyId).then(() => (
+        dispatch(removeStory(storyId))
+    ), (errors) => (
+        dispatch(receiveErrors(errors.responseJSON))
+    ))
+);
