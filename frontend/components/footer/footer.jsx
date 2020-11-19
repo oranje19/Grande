79ae5d7e4b4de45926ed44c320f5a82fada955a7
oranje19 <jasonwintery@gmail.com>
2020-11-19 17:24:43 -0500

    Comment error

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -14,7 +14,7 @@ class Footer extends React.Component {
                         
                         <div className="footer-medium">
                             <h2>Inspired by</h2>
-                            <a href="https://medium.com/">
+                            <a target="_blank" href="https://medium.com/">
                                 <img src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="Medium" />
                             </a>
                         </div>
@@ -22,22 +22,22 @@ class Footer extends React.Component {
 
                     <div className="footer-links">
                         <h2>Discover</h2>
-                        <a href="https://www.epa.gov/">EPA</a>
-                        <a href="https://sustainabledevelopment.un.org/sdgs">SDG Goals</a>
-                        <a href="https://www.aashe.org/">Aashe</a>
-                        <a href="https://www.fema.gov/">FEMA</a>
+                        <a target="_blank" href="https://www.renewableenergyworld.com/">REW</a>
+                        <a target="_blank" href="https://cleantechnica.com/">Clean Technica</a>
+                        <a target="_blank" href="https://alternativeenergy.procon.org/">ProCon</a>
+                        <a target="_blank" href="https://www.rechargenews.com/">Recharge</a>
                     </div>
 
                     <div className="footer-info">
                         <h2>Contact me</h2>
                         <div className="footer-icons">
-                            <a href="https://github.com/oranje19">
-                                <i class="devicon-github-plain colored"></i>
+                            <a target="_blank" href="https://github.com/oranje19">
+                                <i className="devicon-github-plain colored"></i>
                             </a>
-                            <a href="https://www.linkedin.com/in/jasonwintery/">
+                            <a target="_blank" href="https://www.linkedin.com/in/jasonwintery/">
                                 <i className="devicon-linkedin-plain colored"></i>
                             </a>
-                            <a href="http://www.jasonwintery.com/">
+                            <a target="_blank" href="http://www.jasonwintery.com/">
                                 <img src="https://www.flaticon.com/svg/static/icons/svg/841/841364.svg" alt="Portfolio" />
                             </a>
                         </div>
